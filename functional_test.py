16dc21927dc281cab4867163d7b264a8019d884a
mohammadrabetian <tommyrabetian@gmail.com>
2019-11-04 15:26:37 +0330

    wrote our first test case

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,19 @@
+import unittest
 from selenium import webdriver
 
-driver = webdriver.Chrome('/home/ariana/Downloads/chromedriver')
-driver.get('http://localhost:8000')
 
-assert 'Django' in driver.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('./chromedriver')
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('https://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
