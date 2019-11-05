a50c5815103eb015fac5d6b3f4488fedf7d9dd5c
mohammadrabetian <tommyrabetian@gmail.com>
2019-11-05 12:25:03 +0330

    refactored home template related to FT tests

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 import unittest
-from selenium.webdriver.common import keys
+import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
@@ -20,14 +21,14 @@ class NewVisitorTest(unittest.TestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
-            'Enter a to-to item'
+            'Enter a to-do item'
         )
         input_box.send_keys('Buy something')
-        input_box.send_keys(Keys.Enter)
+        input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        row = table.find_elements_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy something' for row in rows)
         )
