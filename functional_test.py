6f29563deeece863ff0a07227edac41257d43819
mohammadrabetian <tommyrabetian@gmail.com>
2019-11-05 11:26:21 +0330

    added more test to our functional test

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 import unittest
+from selenium.webdriver.common import keys
 from selenium import webdriver
 
 
@@ -13,6 +14,23 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-to item'
+        )
+        input_box.send_keys('Buy something')
+        input_box.send_keys(Keys.Enter)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        row = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy something' for row in rows)
+        )
 
 
 if __name__ == '__main__':
